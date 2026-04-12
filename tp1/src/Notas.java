e9c2871b12f46c12e5db943f2d901948c2cfe7bd
lukas <lukasalderetebrito202@gmail.com>
2026-04-12 19:45:13 -0300

    punto1

---
+++ tp1/src/Notas.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Notas {
+    int notas;
+
+
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in
+        );
+
+        System.out.println("introduce la nota del alumno");
+        int notas = sc.nextInt();
+        if (notas <= 4){
+            System.out.println("Desaprobo la materia");
+        }
+        else if (notas >= 4){
+            System.out.println("Aprobo la materia");
+
+        }else if (10 == notas){
+            System.out.println("Es SOBRESALIENTE");
+
+        }
+    }
+}
